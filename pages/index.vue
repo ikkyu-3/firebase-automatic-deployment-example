3e718c16c0649e48b181bc80f8f5829b07b43053
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2020-05-17 15:41:06 +0900

    indexページを変更

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        firebase-automatic-deployment-example
+        Nuxt Project
       </h1>
       <h2 class="subtitle">
         My astounding Nuxt.js project
